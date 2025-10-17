102e69986dcfe7f800d754fd00bddc0eb27097ec
Yu Shing <yushing@universetech.cc>
2025-10-17 11:42:34 +0800

    feat: show default value 0

--- resources/js/screens/dashboard.vue
+++ resources/js/screens/dashboard.vue
@@ -333,7 +333,7 @@
                     <td class="text-muted">{{ supervisor.options.queue.replace(/,/g, ', ') }}</td>
                     <td class="text-end text-muted">{{ countProcesses(supervisor.processes) }}</td>
                     <td class="text-end text-muted">
-                        {{ supervisor.options.concurrency }}
+                        {{ supervisor.options.concurrency || 1 }}
                     </td>
                     <td class="text-end text-muted" v-if="supervisor.options.balance">
                         {{ supervisor.options.balance.charAt(0).toUpperCase() + supervisor.options.balance.slice(1) }}

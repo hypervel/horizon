075142220f122039d4d18b151566afb64eca70fa
Yu Shing <yushing@universetech.cc>
2025-10-22 17:24:41 +0800

    style: fix phpstan

--- src/Console/ForgetFailedCommand.php
+++ src/Console/ForgetFailedCommand.php
@@ -31,7 +31,7 @@ class ForgetFailedCommand extends Command
             do {
                 $failedJobs = collect($repository->getFailed());
 
-                $failedJobs->pluck('id')->each(function ($failedId) use ($repository): void {
+                $failedJobs->pluck('id')->each(function (string $failedId) use ($repository): void {
                     $repository->deleteFailed($failedId);
 
                     if ($this->app->get(FailedJobProviderInterface::class)->forget($failedId)) {
